5399a9f15b850ddeed91006d865420a227c1f5e8
Steven <stevenyam33@gmail.com>
2023-04-04 20:21:17 +1000

    modified:   index.html 	new file:   preloader.js 	modified:   style.css

---
+++ preloader.js
@@ -0,0 +1,13 @@
+const header = document.querySelector("#header");
+const home = document.querySelector("#home");
+const footer = document.querySelector("#footer");
+const preloader = document.querySelector("#preloader")
+
+window.onload = function(){
+    setTimeout(function(){
+        header.classList.remove("hide");
+        home.classList.remove("hide");
+        footer.classList.remove("hide");
+        preloader.classList.add("hide");
+    }, 3000)
+}
