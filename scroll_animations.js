410546da3f7718e7f91d7057db66ee619dd2515d
Steven <stevenyam33@gmail.com>
2023-04-07 03:34:35 +1000

    modified:   scroll_animations.js 	modified:   style.css

--- scroll_animations.js
+++ scroll_animations.js
@@ -1,9 +1,17 @@
-const home = document.querySelector(".home-container");
+// const sections = document.querySelector(".animation");
 
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) =>{
-        if (entry.isIntersecting){
-            entry.target.classList.add("scroll-animation")
-        }
-    })
-})
+const observer = new IntersectionObserver((entries, observer) => {
+    
+    entries.filter(e => e.isIntersecting).forEach((entry) =>{
+        entry.target.classList.add("scroll");
+        observer.unobserve(entry.target);
+    });
+
+},{
+    threshold: 0.2
+});
+
+document.querySelectorAll('.home-container')
+    .forEach(e =>{
+        observer.observe(e)
+    });
