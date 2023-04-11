dab0a58800616de8e3da22f2db4906396916fb27
Steven <stevenyam33@gmail.com>
2023-04-11 20:04:42 +1000

    modified:   index.html 	modified:   randomstring.js 	modified:   style.css

--- randomstring.js
+++ randomstring.js
@@ -1,4 +1,6 @@
 const headerLinkText = document.querySelectorAll(".header-link");
+const button = document.querySelectorAll(".button");
+const resumeButton = document.querySelectorAll(".resume-button");
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
 
 headerLinkText.forEach((text) => {
@@ -28,3 +30,57 @@ headerLinkText.forEach((text) => {
 
 })
 
+
+button.forEach((text) => {
+    
+    text.addEventListener("mouseover", random => {
+        let iterations = 0;
+
+        const interval = setInterval(() => {
+            random.target.innerText = random.target.innerText
+            .split("")
+            .map((letter, index) => {
+                if(index < iterations) {
+                    return random.target.dataset.value[index];
+                }
+
+                return letters[Math.floor(Math.random() * 36)]
+            })
+            .join("");
+
+            if(iterations >= random.target.dataset.value.length){
+                clearInterval(interval);
+            }
+            iterations += 1.5;
+        }, 30)
+
+    })
+
+})
+
+resumeButton.forEach((text) => {
+    
+    text.addEventListener("mouseover", random => {
+        let iterations = 0;
+
+        const interval = setInterval(() => {
+            random.target.innerText = random.target.innerText
+            .split("")
+            .map((letter, index) => {
+                if(index < iterations) {
+                    return random.target.dataset.value[index];
+                }
+
+                return letters[Math.floor(Math.random() * 36)]
+            })
+            .join("");
+
+            if(iterations >= random.target.dataset.value.length){
+                clearInterval(interval);
+            }
+            iterations += 1 / 2;
+        }, 30)
+
+    })
+
+})
