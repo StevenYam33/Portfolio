d8a0b7f38397aa5b2abb14531bac181c4d0ae42f
Steven <stevenyam33@gmail.com>
2023-04-11 20:20:01 +1000

    modified:   nav_mobile.js

--- nav_mobile.js
+++ nav_mobile.js
@@ -5,37 +5,23 @@ const headerLink = document.querySelector(".header-links");
 const body = document.querySelector("body");
 
 
-headerButton.addEventListener('click', () => {
-    const visibility = headerList.getAttribute('data-visible');
-
-    if (visibility === "false") {
-        headerList.setAttribute("data-visible", true);
-        body.setAttribute("aria-expanded", true);
-        headerButton.setAttribute("aria-expanded", true);
-        main.setAttribute("aria-expanded", true);
-    } else if (visibility === "true") {
-        headerList.setAttribute("data-visible", false);
-        body.setAttribute("aria-expanded", false);
-        headerButton.setAttribute("aria-expanded", false);
-        main.setAttribute("aria-expanded", false);
-    }
-})
-
-headerLink.addEventListener('click', () =>{
-    const visibility = headerList.getAttribute('data-visible');
-
-    if (visibility === "false") {
-        headerList.setAttribute("data-visible", true);
-        body.setAttribute("aria-expanded", true);
-        headerButton.setAttribute("aria-expanded", true);
-        main.setAttribute("aria-expanded", true);
-    } else if (visibility === "true") {
-        headerList.setAttribute("data-visible", false);
-        body.setAttribute("aria-expanded", false);
-        headerButton.setAttribute("aria-expanded", false);
-        main.setAttribute("aria-expanded", false);
-    }
-})
+[headerButton, headerLink].forEach(function(e){
+    e.addEventListener('click', () => {
+        const visibility = headerList.getAttribute('data-visible');
+    
+        if (visibility === "false") {
+            headerList.setAttribute("data-visible", true);
+            body.setAttribute("aria-expanded", true);
+            headerButton.setAttribute("aria-expanded", true);
+            main.setAttribute("aria-expanded", true);
+        } else if (visibility === "true") {
+            headerList.setAttribute("data-visible", false);
+            body.setAttribute("aria-expanded", false);
+            headerButton.setAttribute("aria-expanded", false);
+            main.setAttribute("aria-expanded", false);
+        }
+    })
+});
 
 main.addEventListener('click', () =>{
     const visibility = headerList.getAttribute('data-visible');
