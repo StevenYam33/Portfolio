1ddb079f7a81a635891c257146dab1524bd1ebe6
Steven <stevenyam33@gmail.com>
2023-04-19 02:57:50 +1000

    modified:   style.css 	modified:   title.js

--- title.js
+++ title.js
@@ -4,21 +4,21 @@ const titleLoop = function(){
             
     setTimeout (()=>{
         title.textContent = "Front-End Web Developer";
-        document.documentElement.style.setProperty('--word-count', '46');
+        document.documentElement.style.setProperty('--word-count', '23');
     },3000)
 
     setTimeout(() => {
         title.textContent = "Mobile App Developer";
-        document.documentElement.style.setProperty('--word-count', '40');
+        document.documentElement.style.setProperty('--word-count', '20');
     },6000);
 
     setTimeout(() => {
         title.textContent = "Financial Technology Specialist";
-        document.documentElement.style.setProperty('--word-count', '62');
+        document.documentElement.style.setProperty('--word-count', '31');
     },12000);
     setTimeout(() => {
         title.textContent = "Front-End Web Developer"
-        document.documentElement.style.setProperty('--word-count', '46');
+        document.documentElement.style.setProperty('--word-count', '23');
     },18000);
 
 };
