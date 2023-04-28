23b6b1dc5fed15470fb1809a399f0ec342545711
Steven <stevenyam33@gmail.com>
2023-04-29 02:40:17 +1000

    modified:   cursor.js 	modified:   index.html 	modified:   style.css

--- cursor.js
+++ cursor.js
@@ -11,38 +11,19 @@ window.addEventListener("pointermove", e =>{
 });
 // not display when outside
 window.addEventListener("pointerout", ()=>{
-    console.log("out");
     cursor.style.display = "none";
 });
 
 // moblie touch events
 window.addEventListener("pointerdown", e =>{
-    console.log(e);
-    console.log("touchStart");
     cursor.style.display = "block";
     cursor.style.left = e.pageX + "px";
     cursor.style.top = e.pageY + "px";
 });
 
 window.addEventListener("pointercancel", ()=>{
-    console.log("cancel");
     cursor.style.display = "none";
 });
-
-
-// window.addEventListener("touchmove", ()=>{
-//     console.log("touchMove");
-
-//     cursor.style.display = "block";
-//     cursor.style.left = e.pageX + "px";
-//     cursor.style.top = e.pageY + "px";
-// })
-
-// window.addEventListener("mousedown", ()=>{
-//     console.log("touchEnd");
-
-//     cursor.style.display = "none";
-// });
 
 // Set color when hover to the elements
 [preloader, profilePic, expTab, formContianer, footer].forEach(function(e){
