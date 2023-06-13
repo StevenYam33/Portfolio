642c08a3400e1729d69f4a017d3093436bee2ae9
Steven <stevenyam33@gmail.com>
2023-06-13 16:51:46 +1000

    modified:   cursor.js 	modified:   style.css     fixed touchscreen cursor

--- cursor.js
+++ cursor.js
@@ -3,63 +3,111 @@ const moblieMenu = document.querySelector("#header-navigation-menu");
 const profilePic = document.querySelector("#profile-pic");
 const expTab = document.querySelector(".experience-tab");
 
+var isTouching = false;
+var triggeringElements = [".preloader", "#profilePic", ".experience-tab", ".contact-main", ".footer-main", "#header-navigation-menu"]
+
+
 // Set cursor movement
 window.addEventListener("pointermove", e =>{
     cursor.style.display = "block";
-    cursor.style.left = e.pageX + "px";
-    cursor.style.top = e.pageY + "px";
+    cursor.style.left = e.clientX + "px";
+    cursor.style.top = e.clientY + "px";
 });
+
 // not display when outside
 window.addEventListener("pointerout", ()=>{
-    cursor.style.display = "none";
+    if (!isTouching) {
+        cursor.style.display = "none";
+    }
 });
 
-// moblie touch events
-window.addEventListener("pointerdown", e =>{
+// touch starts
+window.addEventListener("touchstart", ()=>{
     cursor.style.display = "block";
-    cursor.style.left = e.pageX + "px";
-    cursor.style.top = e.pageY + "px";
+    isTouching = true;
 });
 
-// moblie scroll events
-window.addEventListener('scroll', ()=>{
-    cursor.style.display = "block";
-    cursor.style.left = e.pageX + "px";
-    cursor.style.top = e.pageY + "px";
+window.addEventListener("touchmove", e=>{
+    var touch = e.touches[0];
+    var mouseX = touch.clientX;
+    var mouseY = touch.clientY;
+  
+    cursor.style.left = mouseX + "px";
+    cursor.style.top = mouseY + "px";
 });
 
-window.addEventListener("pointercancel", ()=>{
+window.addEventListener("touchend", ()=> {
+    isTouching = false;
     cursor.style.display = "none";
 });
 
 // Set color when hover to the elements
-[preloader, profilePic, expTab, formContianer, footer].forEach(function(e){
+[preloader, profilePic, expTab, formContianer, footer].forEach(function(elements){
 
-    e.addEventListener("pointermove", ()=>{
-        cursor.style.boxShadow = "0 0 0 1px var(--background)";
-    });
+    ["pointermove", "pointerdown"].forEach(pointer => 
+        elements.addEventListener(pointer, cursorColorChange)
+    );
     
-    e.addEventListener("pointerout", ()=>{
-        cursor.style.boxShadow = "0 0 0 1px var(--text)";
+    elements.addEventListener("pointerout", ()=>{
+        cursorColorReset();
     });
 
 });
 
 function moblieMenuColor(){
-
     const width = Math.max(document.clientWidth || 0, window.innerWidth || 0);
 
     if (width <= 850){
-        moblieMenu.addEventListener("pointermove", ()=>{
-            cursor.style.boxShadow = "0 0 0 1px var(--background)";
-        });
+
+
+        // ["pointermove"].forEach(e => 
+        //     moblieMenu.addEventListener(e, cursorColorReverse)
+        // );
         
-        moblieMenu.addEventListener("pointerout", ()=>{
-            cursor.style.boxShadow = "0 0 0 1px var(--text)";
-        });
+        // ["pointerout"].forEach(e =>
+        //     moblieMenu.addEventListener(e, cursorColorReset)
+        // );
+
+        window.addEventListener('touchmove', function(event) {
+            var touch = event.touches[0];
+            var targetElement = document.elementFromPoint(touch.clientX, touch.clientY);
+          
+            if (targetElement) {
+              var isTriggeringElement = triggeringElements.some(function(selector) {
+                if (selector.charAt(0) === '#') {
+                  return targetElement.id === selector.substr(1);
+                } else {
+                  return targetElement.classList.contains(selector.substr(1));
+                }
+              });
+          
+              if (isTriggeringElement) {
+                cursorColorChange();
+              } else {
+                cursorColorReset();
+              }
+            }
+          });
+
+            // ["pointermove", "pointerdown"].forEach(pointer => 
+            //     elements.addEventListener(pointer, cursorColorReverse)
+            // );
+            
+            // elements.addEventListener("pointerout", ()=>{
+            //     cursorColorReset();
+            // });
+        
     };
 
 };
+
+function cursorColorChange() {
+    cursor.style.boxShadow = "0 0 0 1px var(--background)";
+}
+
+function cursorColorReset() {
+    cursor.style.boxShadow = "0 0 0 1px var(--text)";
+}
 
 document.onload = moblieMenuColor();
 window.onresize = () => moblieMenuColor();
